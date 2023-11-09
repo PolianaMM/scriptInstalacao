769b1c0a1974eaabcd334098cdee0fafd481f0a0
Poliana Micheline Militao <pmmilitao@simpress.com.br>
2023-11-09 09:53:39 -0300

    Verificações de etapas adicionadas, mais informação ao usuário, criação de imagem dockerfile

--- install.sh
+++ install.sh
@@ -20,27 +20,64 @@ echo "..."
 echo "Pacotes atualizados!"
 echo "..."
 
-#Instalando e iniciando docker
-sleep 4
+sleep 3
 echo "..."  
-echo "Estamos criando o Docker" 
-echo "Não se preocupe não fará alterações em seu computador!"
-echo "..."  
-sudo apt install docker.io -y
-docker --version
-sudo systemctl start docker
-sudo systemctl enable docker
+echo "Criando Docker" 
+echo "Não se preocupe, esse processo não afetará seus aplicativos atuais"
+echo "..."
+if sudo apt install docker.io -y && sudo systemctl start docker && sudo systemctl enable docker; then
+echo "Docker instalado e iniciado com sucesso!"
+else
+echo "Erro ao instalar o Docker."
+exit 1
+fi
 
 #Criação do container Banco de Dados e instalação
-sleep 6
+sleep 4
+
 echo "..."  
 echo "Instalando Mysql" 
 echo "Aguarde um instante enquanto fazemos as configurações..." 
+echo "Não se preocupe, esse processo não afetará seus aplicativos atuais"
 echo "..."  
-sudo docker pull mysql:8
-sudo docker images
-sudo docker run -d -p 3306:3306 --name noctuBD -e "MYSQL_DATABASE=aluno" -e "MYSQL_ROOT_PASSWORD=aluno" mysql:8
-sudo service mysql start
+
+if sudo docker pull mysql:8; then
+    echo "Docker image do MySQL 8 baixada com sucesso!"
+else
+    echo "Erro ao baixar a imagem do MySQL. Entre em contato com a equipe Noct.u."
+    exit 1
+fi
+
+# Verificar a existência da imagem baixada
+if sudo docker images | grep -q "mysql"; then
+    echo "Imagem do MySQL 8 encontrada."
+else
+    echo "Imagem do MySQL 8 não encontrada. Encerrando o script. Entre em contato com a equipe Noct.u."
+    exit 1
+fi
+
+if sudo docker run -d -p 3306:3306 --name noctuBD -e "MYSQL_DATABASE=aluno" -e "MYSQL_ROOT_PASSWORD=aluno" mysql:8; then
+    echo "Container do Banco de Dados criado com sucesso!"
+else
+    echo "Erro ao criar o container do Banco de dados. Entre em contato com a equipe Noct.u."
+    exit 1
+fi
+
+# Verificando se o container está em execução
+if sudo docker ps -a | grep -q "noctuBD"; then
+    echo "Container do Banco de Dados está em execução."
+else
+    echo "Erro ao iniciar o container do Banco de dados. Entre em contato com a equipe Noct.u."
+    exit 1
+fi
+
+# Iniciando o serviço do MySQL no container
+if sudo service mysql start; then
+    echo "Banco de dados iniciado com sucesso!"
+else
+    echo "Erro ao iniciar o Banco de dados. Entre em contato com a equipe Noct.u."
+    exit 1
+fi
 
 #Lista de containers assossiados
 echo "..." 
@@ -49,32 +86,71 @@ echo "..."
 sudo docker ps -a
 
 #Importando o script .sql para container do banco
-sleep 8
+sleep 6
 echo "..."
-echo "Acessando Banco de Dados"  
+echo "Configurando Banco de Dados"  
 echo "..."
-sudo docker exec -i noctuBD mysql -u aluno -p aluno root < confBanco.sql
+if sudo docker exec -i noctuBD mysql -u aluno -p < confBanco.sql; then
+echo "Script SQL executado com sucesso no banco de dados!"
+else
+echo "Erro ao executar o script SQL."
+exit 1
+fi
+# Conexão com Banco local
+# mysql -h 127.0.0.1 -P 3306 -u aluno -p aluno -D aluno  
 
 # Verificando e executando o Banco de Dados
-sleep 9
+sleep 7
 echo "..."
 echo "Iniciando Banco de Dados" 
-echo "..." 
-sudo docker mysql -u root -p -h localhost
+echo "..."
+if sudo docker exec noctuBD service mysql start; then
+echo "Banco de Dados Iniciado com sucesso!"
+else
+echo "Erro ao iniciar Banco de dados"
+exit 1
+fi
 
-sleep 6
+sleep 8
 echo "..."  
 echo "Instalando Java 17"  
-echo "Não se preocupe não irá fazer modificações nas aplicações atuais!"
+echo "Não se preocupe, esse processo não afetará seus aplicativos atuais"
 echo "..."  
-docker build -t java_17_image .
-sudo docker run -it --name java_container java_17_image /bin/bash
+
+if docker build -t java_17_image .; then
+    echo "Construção da imagem do Java 17 bem-sucedida!"
+else
+    echo "Erro durante a construção da imagem do Java 17. Entre em contato com a equipe Noct.u"
+    exit 1
+fi
+
+if sudo docker run -it --name containerJava java_17_image /bin/bash; then
+    echo "Container Java 17 iniciado com sucesso!"
+else
+    echo "Erro ao iniciar o container do Java 17. Entre em contato com a equipe Noct.u."
+    exit 1
+fi
 
 #Instalação do .jar
-sleep 4
+sleep 9
 echo "..."
 echo "Instalando aplicação Noct.u"
 echo "Logo inicializaremos a sua central de monitoramento"
 echo "..." 
 echo "Estamos instalando a aplicação"
-curl -LJO https://github.com/Noct-U/Noct.u/raw/main/java/out/artifacts/noctu_looca_jar/noctu-looca.jar
+
+if curl -LJO https://github.com/Noct-U/Noct.u/raw/main/java/out/artifacts/noctu_looca_jar/noctu-looca.jar; then
+    if [ -f "noctu-looca.jar" ]; then
+        echo "Arquivo noctu-looca.jar baixado e instalado com sucesso!"
+        # Executando a aplicação somente se o download do arquivo foi bem-sucedido
+        java -jar noctu-looca.jar
+    else
+        echo "O arquivo noctu-looca.jar não foi encontrado."
+        exit 1
+    fi
+else
+    echo "Erro ao baixar o arquivo noctu-looca.jar. Entre em contato com a equipe Noct.u"
+    exit 1
+fi
+
+echo "BEM - VINDO A NOCT.U!"
