d284f50cf8f736983e290aa9cbfc436dd6d78d7f
Poliana Micheline Militao <pmmilitao@simpress.com.br>
2023-11-10 11:00:01 -0300

    diretorio

--- install.sh
+++ install.sh
@@ -77,7 +77,7 @@ echo "Instalando Mysql"
 echo "Aguarde um instante enquanto fazemos as configurações..." 
 echo "Não se preocupe, esse processo não afetará seus aplicativos atuais"
 echo "..."  
-if sudo docker build --network=host -t bancodedados -f ../scriptInstalacao/dockerfileBanco/Dockerfile ..; then
+if sudo docker build --network=host -t bancodedados -f ../noctu/scriptInstalacao/dockerfileBanco/Dockerfile ..; then
     echo "..."
     echo "Imagem do MySQL construída com sucesso!"
     echo "..."
