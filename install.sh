598283bb9577a0fffd51dffe0fb1397e658f3b2b
Poliana Micheline Militao <pmmilitao@simpress.com.br>
2023-11-13 12:58:17 -0300

    caminho

--- install.sh
+++ install.sh
@@ -103,9 +103,9 @@ sudo systemctl enable mysql
 echo "..."
 echo "Instalando aplicações finalizadas!"
 echo "..."
-if sudo docker exec -i Noctu mysql -u aluno -paluno </docker-entrypoint-initdb.d/confBanco.sql &&
+if sudo docker exec -i Noctu mysql -u aluno -paluno </home/ubuntu/scriptInstalacao/confBanco.sql &&
    sudo apt install mysql-client -y &&
-   sudo mysql -u aluno -paluno -h 127.0.0.1 -P 3306 </docker-entrypoint-initdb.d/confBanco.sql; then
+   sudo mysql -u aluno -paluno -h 127.0.0.1 -P 3306 </home/ubuntu/scriptInstalacao/confBanco.sql; then
     echo "..."
     echo "Script SQL executado com sucesso no banco de dados!"
     echo "..."
