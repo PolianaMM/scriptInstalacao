791dcf63527d75ecf5699d668a5c2ae7cc783f3f
Poliana Micheline Militao <pmmilitao@simpress.com.br>
2023-11-14 10:44:46 -0300

    explicação validação java 17

--- java.sh
+++ java.sh
@@ -11,6 +11,24 @@ sleep 5
 echo "..."
 echo "Verificando se você possui o Java instalado na sua máquina!"
 echo "..."
+
+#validação do java: 
+# which java > /dev/null 2>&1 = verifica se o comando java está disponível no sistema;
+# which java = procura o java;
+# > /dev/null 2>&1= ignora qualquer mensagem de erro que possa ter caso não encontre o java. se estiver disponível ele vai pro passo do "then", se não ele segue para o próximo passo;
+
+# java_version=$(java -version 2>&1 | head -n 1 | awk -F '"' '{print $2}') = validação da versão do sistema;
+# java_version=$(...) = é uma "variável", ele vai armazemar o que o comando a seguir realizar;
+# java -version 2>&1 = verifica a versão do java e redireciona para a saida padrão;
+# | head -n 1 = pega a primeira linha da saida;
+# | awk -F '"' '{print $2}' = extrai o texto entre aspas que contem a versão do java 
+
+# "$(echo -e "17\n$java_version" | sort -V | head -n 1)" == "17" = Cria uma string contendo as versões "17" e a versão do Java, cada uma em uma linha separada.
+# echo -e "17\n$java_version" = aqui ele coloca a versão do java 17 e a que foi encontrada, uma embaixo da outra;
+# sort -V = ele ordena as informações 
+# head -n 1 = pega informação da primeira linha, que é a versão mais alta;
+# [ "$(…)" == "17" ] = compara a versão mais alta que acho e compara com a 17
+
 if which java > /dev/null 2>&1; then
   java_version=$(java -version 2>&1 | head -n 1 | awk -F '"' '{print $2}')
 
@@ -59,7 +77,9 @@ echo "..."
 echo "Estamos instalando a aplicação"
 echo "..."
 if [ ! -f "$jar_nome" ]; then
+  echo "..." 
   echo "Baixando a aplicação JAR..."
+  echo "..." 
   sudo apt install wget -y
   sleep 5
   sudo wget "$jar_url" -O "$jar_nome"
